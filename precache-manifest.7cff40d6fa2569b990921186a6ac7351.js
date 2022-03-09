aa2f74588832034bc2907d433dea11fb4ac86f1c
enesates <enes@enesates.com>
2022-03-09 03:59:37 +0100

    Updates

---
+++ precache-manifest.7cff40d6fa2569b990921186a6ac7351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e966db7bfb606cbb52188cf73824495",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b88f5af63dc8610da2ef",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3f1513a840d8458bca00",
+    "url": "./static/css/main.b0fa59df.chunk.css"
+  },
+  {
+    "revision": "b88f5af63dc8610da2ef",
+    "url": "./static/js/2.09238dc2.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "./static/js/2.09238dc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f1513a840d8458bca00",
+    "url": "./static/js/main.302589de.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.ee50bc0a1fbb4aafab17a8b6801d6718.js"
+  "./precache-manifest.7cff40d6fa2569b990921186a6ac7351.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
